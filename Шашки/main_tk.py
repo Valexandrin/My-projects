474330a03d5d7e02f1b1a1c579177f01041d2c48
Vadim Alexandrin <alex-vadim@mail.ru>
2021-03-16 22:44:49 +0300

    Checkers moving by clicking added. Dict with available positions for moving for all "black" cells added.

--- Шашки/main_tk.py
+++ Шашки/main_tk.py
@@ -15,46 +15,92 @@ canv.pack()
 wh_img = tk.PhotoImage(file="white.png")
 bl_img = tk.PhotoImage(file="black.png")
 
-field = []
+book = {}
+player = 1
+selected = []
+available = []
 
 
 class Cell:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.figure = 0
+        self.status = 0
         self.color = BLACK if (x + y) % 2 != 0 else WHITE
         self.id = canv.create_rectangle((x * SIZE, y * SIZE, (x + 1) * SIZE, (y + 1) * SIZE),
                                         fill=self.color)
 
-    def clicked(self):
-        canv.itemconfig(self.id, outline="green", width=7)
+    def set_checker(self):
+        if self.status == 2:
+            canv.create_image(self.x * SIZE, self.y * SIZE, image=bl_img, anchor=tk.NW)
+        elif self.status == 1:
+            canv.create_image(self.x * SIZE, self.y * SIZE, image=wh_img, anchor=tk.NW)
+
+    def select(self, color):
+        canv.itemconfig(self.id, outline=color, width=7)
+
+    def released(self):
+        canv.itemconfig(self.id, outline="black", width=1)
+
+    def move(self, finish_cell):
+        self.x = finish_cell.x
+        self.y = finish_cell.y
+        self.status = 0
+        finish_cell.status = 1
+        finish_cell.set_checker()  # TODO: add removing checker from old position
+        self.released()
+        [available.pop().released() for _ in range(len(available))]
 
 
-for cell_row in range(0, 8):
-    for cell_col in range(0, 8):
-        cell = Cell(cell_col, cell_row)
-        if cell_row < 3 and cell.color == BLACK:
-            cell.figure = 2
-        if cell_row > 4 and cell.color == BLACK:
-            cell.figure = 1
-        field += [cell]
+field = [[Cell(j, i) for j in range(0, 8)] for i in range(0, 8)]
 
-for cell in field:
-    if cell.figure == 2:
-        checker = canv.create_image(cell.x * SIZE, cell.y * SIZE, image=bl_img, anchor=tk.NW)
-    elif cell.figure == 1:
-        checker = canv.create_image(cell.x * SIZE, cell.y * SIZE, image=wh_img, anchor=tk.NW)
+for row in field:
+    for cell in row:
+        if cell.y < 3 and cell.color == BLACK:
+            cell.status = 2
+            cell.set_checker()
+        if cell.y > 4 and cell.color == BLACK:
+            cell.status = 1
+            cell.set_checker()
 
+# dict of neighbors of all black cells
+for i in range(8):
+    for j in range(8):
+        if field[i][j].color == BLACK:
+            if i == 0:
+                book[i, j] = [None, None]
+            if j == 0:
+                book[i, j] = [None, field[i - 1][j + 1]]
+            elif j == 7:
+                book[i, j] = [field[i - 1][j - 1], None]
+            else:
+                book[i, j] = [field[i - 1][j - 1], field[i - 1][j + 1]]
+
+
+def check_action(event):
+    if field[event.y // SIZE][event.x // SIZE] in available:
+        finish_cell = available[available.index(field[event.y // SIZE][event.x // SIZE])]
+        selected.pop().move(finish_cell)
+    # TODO: add actions if clicking to cells outside of "available"
 
 def grip(event):
-    for cell in field:
-        if cell.x == event.x // 100 and cell.y == event.y // 100 and cell.figure == 1:
-            print(cell.x, cell.y)
-            cell.clicked()
+    for cell in book[event.y // SIZE, event.x // SIZE]:
+        if cell.status == 0:
+            cell.select("yellow")
+            available.append(cell)
+    if available:
+        field[event.y // SIZE][event.x // SIZE].select("green")
+    selected.append(field[event.y // SIZE][event.x // SIZE])
 
 
 while True:
-    canv.bind("<Button-1>", grip)
+    if player == 1:
+        if not selected:
+            canv.bind("<Button-1>", grip)
+        else:
+            canv.bind("<Button-1>", check_action)
+    else:
+        pass
+
     canv.update()
     time.sleep(0.03)
